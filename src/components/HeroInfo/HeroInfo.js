501f10d5cdb80cd4d21decb807b4ee0f38a689ff
mrideout1985 <mrideout1985@gmail.com>
2020-04-21 22:18:33 +0100

    updated

--- src/components/HeroInfo/HeroInfo.js
+++ src/components/HeroInfo/HeroInfo.js
@@ -1,30 +1,59 @@
-import React from 'react'
+import React from "react";
 // import useHeros from "../../hooks/useHeros"
-import { useParams } from 'react-router-dom'
-import  useHero  from '../../hooks/useHero'
-import styles from './HeroInfo.module.scss'
+import { useParams } from "react-router-dom";
+import useHero from "../../hooks/useHero";
+import styles from "./HeroInfo.module.scss";
 
+export default function HeroInfo() {
+	let { id } = useParams();
+	const hero = useHero(id);
 
- export default function HeroInfo(){
-
-    let {id} = useParams()
-    const hero = useHero(id)
-    
-
-    return (
-        <div className={styles["container__hero"]}>
-            <img src={hero?.image?.url !== undefined 
-            ? hero?.image?.url : 'no img'} />
-            <h1>{hero?.name}</h1>
-            {/* <h2>{`Int: ${hero?.powerstats?.intelligence}`}</h2>
-            <h2>{`Str: ${hero?.powerstats?.strength}`}</h2>
-            <h2>{`Spd: ${hero?.powerstats?.speed}`}</h2>
-            <h2>{`Dur: ${hero?.powerstats?.durability}`}</h2>
-            <h2>{`Pow: ${hero?.powerstats?.power}`}</h2> */}
-
-
-        </div>
-        
-
-    )
+	return (
+		<div className={styles["hero-page"]}>
+			<div
+				className={styles["header-container"]}
+				style={{
+					backgroundImage: `url(${hero?.image?.url})`,
+					backgroundSize: "cover",
+				}}
+			>
+				<div className={styles["header"]}>
+					<img
+						src={
+							hero?.image?.url !== undefined
+								? hero?.image?.url
+								: "no img"
+						}
+						alt=""
+					/>
+					<div className={styles["details"]}>
+						<ul>
+							<div>
+								<h1>{hero?.name}</h1>
+								<h3>
+									Aliases: {`"${hero?.biography.aliases}"`}
+								</h3>
+								<div>
+									<h3>
+										Publisher: {hero?.biography?.publisher}
+									</h3>
+									<div className={styles["overview"]}>
+										asdfasdf
+									</div>
+								</div>
+							</div>
+						</ul>
+					</div>
+				</div>
+				//{" "}
+				{/* <h2>{`Int: ${hero?.powerstats?.intelligence}`}</h2>
+		//     <h2>{`Str: ${hero?.powerstats?.strength}`}</h2>
+		//     <h2>{`Spd: ${hero?.powerstats?.speed}`}</h2>
+		//     <h2>{`Dur: ${hero?.powerstats?.durability}`}</h2>
+		//     <h2>{`Pow: ${hero?.powerstats?.power}`}</h2> */}
+				//{" "}
+			</div>
+			//{" "}
+		</div>
+	);
 }
--- src/components/search/Search.js
+++ src/components/search/Search.js
@@ -1,86 +1,85 @@
-import React, {useState, useEffect, useRef} from 'react'
-import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom'
-import { useDebounce } from 'react-use'
-import { useClickAway } from 'react-use'
-import styles from "./Search.module.scss"
-import Axios from "axios"
-import useHeros from "../../hooks/useHeros"
+import React, { useState, useEffect, useRef } from "react";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import { useDebounce } from "react-use";
+import { useClickAway } from "react-use";
+import styles from "./Search.module.scss";
+import Axios from "axios";
+import useHeros from "../../hooks/useHeros";
 
 export const Search = () => {
+	const [input, setInput] = useState("");
+	const [search, setSearch] = useState("");
+	const heros = useHeros(search);
+	const [isOpen, setIsOpen] = useState(false);
+	const ref = useRef(null);
 
-    const [input, setInput] = useState('')
-    const [search, setSearch] = useState('')
-    const heros = useHeros(search)
-    const [isOpen, setIsOpen] = useState(false)
-    const ref = useRef(null)
+	useClickAway(ref, () => {
+		console.log("OUTSIDE CLICKED");
+		setIsOpen(false);
+	});
 
-    useClickAway(ref, () => {
-        console.log('OUTSIDE CLICKED')
-        setIsOpen(false)
-    })
+	const [] = useDebounce(
+		() => {
+			setSearch(input);
+			console.log("debouncing");
+		},
+		500,
+		[input]
+	);
 
-    const [] = useDebounce(
-        () => {
-            setSearch(input)
-            console.log('debouncing')
-        },
-        500,
-        [input]
-    )
+	const handleSubmit = (e) => {
+		e.preventDefault();
+	};
 
-    const handleSubmit = (e) => {
-        e.preventDefault()
-    }
+	const handleChange = (e) => {
+		e.preventDefault();
+		setInput(e.target.value);
+	};
 
-    const handleChange = (e) => {
-        e.preventDefault()
-        setInput(e.target.value)
-    }
+	const heroList = (props) => {
+		let results = [];
+		if (heros !== undefined) {
+			if (Object.keys(props).length > 2) {
+				for (let i = 0; i < 5; i++) {
+					let entry = Object.values(props)[2];
+					results.push(entry[i]);
+				}
+			}
+			return results.map((el, key) => {
+				return (
+					<Link to={`/hero-info/${el?.id}`}>
+						<li key={key}>
+							<img src={el?.image.url} />
 
-    const heroList = (props) => {
-        let results = []
-        if (heros !== undefined) {
-            if (Object.keys(props).length > 2) {
-                for (let i = 0; i < 5; i++ ) {
-                    let entry = Object.values(props)[2]
-                    results.push(entry[i])
-                }
-            }
-            return results.map((el, key) => {
-                return (
-                    <Link to={`/hero-info/${el?.id}`}>
-                    <div className={styles["list"]}> 
-                        <li key={key}>
-                        <img src={el?.image.url}/>
-                        <h1>{el?.name}</h1>
-                
-                        </li>
-                    </div>
-                    </Link>
-                )
-            })
-        }
+							<div className={styles["info"]}>
+								<div className={styles["hero"]}>{el?.name}</div>
+							</div>
+						</li>
+					</Link>
+				);
+			});
+		}
+	};
 
-    }
+	return (
+		<form className={styles.form} onSubmit={handleSubmit}>
+			{console.log(heros)}
+			<div className={styles.search}>
+				<div className={styles.input}>
+					<input
+						className={styles["input"]}
+						id="search"
+						type="text"
+						value={input}
+						autoComplete="off"
+						onChange={handleChange}
+						onFocus={() => setIsOpen(true)}
+					/>
+					<ul ref={ref}>{isOpen && heroList(heros)}</ul>
+				</div>
+			</div>
+		</form>
+	);
+};
 
-    return (
-        <form className={styles.form} onSubmit={handleSubmit}>
-            {console.log(heros)}
-            <div className={styles.search}>
-                <div className={styles.input}>
-                    <input
-                        id="search"
-                        type="text"
-                        value={input}
-                        autoComplete="off"
-                        onChange={handleChange}
-                        onFocus={() => setIsOpen(true)}
-                    />
-                    <ul ref={ref}>{isOpen && heroList(heros)}</ul>
-                </div>
-            </div>
-        </form>
-    )
-}
-
-export default Search
+export default Search;
